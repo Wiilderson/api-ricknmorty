52ada1edc4610d8f8c077a946361e8b8d1cf194e
wilderson <wilderson@uitecnologia.com.br>
2024-06-25 15:23:41 -0300

    new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CardDetailsComponent } from './components/card-details/card-details.component';
 import { EpisodesComponent } from './pages/episodes/episodes.component';
+import { PlacesComponent } from './pages/places/places.component';
 
 export const routes: Routes = [
     { 'path': '', component: HomeComponent },
     { 'path': 'details/:id', component: CardDetailsComponent },
-    { 'path': 'episodes', component: EpisodesComponent }
+    { 'path': 'episodes', component: EpisodesComponent },
+    { 'path': 'places', component: PlacesComponent }
 ];
